99a239cc563cae60170e764e1781aa6549267cc7
201244048 <vochelle@naver.com>
2016-11-18 15:09:06 +0900

    Create Project

--- app/src/main/java/com/example/ryuilhan/select_test/MainActivity.java
+++ app/src/main/java/com/example/ryuilhan/select_test/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.ryuilhan.select_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox CH1;
+    boolean BData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        CH1 = (CheckBox)CH1.findViewById(R.id.checkBox);
+        CH1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(CH1.isChecked()){
+                    BData = true;
+                }
+                else {
+                    BData = false;
+                }
+            }
+        });
+
+
+
     }
 }
+
